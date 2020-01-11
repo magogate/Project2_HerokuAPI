db9c7e51fb7c33856beb8f328a35097c6f0c7fdb
magogate <mandargogate@outlook.com>
2020-01-10 22:37:07 -0500

    Heroku API Files

---
+++ Project2_Api2/app.py
@@ -0,0 +1,68 @@
+# Created By: 
+#     Grettel Canepari
+#     Katherine Lee
+#     Mandar Gogate
+#     Petra Alex
+#     Preet Puri
+#     Sweta Shekhar
+# Created On: 01/01/2020
+# Updated On: 01/10/2020
+
+#https://stackoverflow.com/questions/31252791/flask-importerror-no-module-named-flask
+#https://stackoverflow.com/questions/10572498/importerror-no-module-named-sqlalchemy
+
+from flask import Flask, jsonify, render_template, flash
+import dao as db
+# import webScrapPopulation as webscrap
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route("/")
+def index():
+    htmlTag ="""<html>
+                <table>
+                
+                    <tr>
+                        <td> To get city json - use </td>
+                        <td> /city </td>
+                    </tr>
+                    <tr>
+                        <td> To get county json - use </td>
+                        <td> /county </td>
+                    </tr>
+                    <tr>
+                        <td> To load city json - use </td>
+                        <td> /loadCity </td>
+                    </tr>
+                    <tr>
+                        <td> To load county json - use </td>
+                        <td> /loadCounty </td>
+                    </tr>
+                
+                <table>
+                <html>
+            """
+    return htmlTag
+
+@app.route("/city")
+def cityPopulation():
+    return db.getCityPopulation()
+
+@app.route("/county")
+def countyPopulation():
+    return db.getCountyPopulation()
+
+# @app.route("/loadCounty")
+# def loadCountyPopulation():
+#     webscrap.populationByCounties()
+#     return "County data loaded successfully.."
+
+# @app.route("/loadCity")
+# def loadCityPopulation():
+#     webscrap.populationByCities()
+#     return "City data loaded successfullly.."
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run()
+    
---
+++ Project2_Api2/dao.py
@@ -0,0 +1,67 @@
+# Created By: 
+#     Grettel Canepari
+#     Katherine Lee
+#     Mandar Gogate
+#     Petra Alex
+#     Preet Puri
+#     Sweta Shekhar
+# Created On: 01/01/2020
+# Updated On: 01/10/2020
+
+
+from pymongo import MongoClient
+import pandas as pd
+
+def getConnection():
+    # conn = MongoClient('localhost', 27017)
+    connString = "mongodb+srv://mandargogate:Password123@cluster0-x4wnf.mongodb.net/test?retryWrites=true&w=majority"
+    
+    conn = MongoClient(connString, 27017)
+    
+    conn.population
+    return conn.population
+
+# def insertCountyPopulation(counties_list):
+#     db = getConnection()
+
+#     countyPopulation = list(db.county_population.find())
+#     if len(countyPopulation) < 1:
+#         db.county_population.insert(counties_list)
+#     else:
+#         # https://www.w3schools.com/python/python_mongodb_delete.asp
+#         db.county_population.delete_many({})
+#         db.county_population.insert(counties_list)
+
+# def insertCityPopulation(cities_list):
+#     db = getConnection()
+
+#     cityPopulation = list(db.city_population.find())
+#     if len(cityPopulation) < 1:
+#         db.city_population.insert(cities_list)
+#     else:
+#         # https://www.w3schools.com/python/python_mongodb_delete.asp
+#         db.city_population.delete_many({})
+#         db.city_population.insert(cities_list)
+
+def getCountyPopulation():
+    db = getConnection()    
+    # https://stackoverflow.com/questions/49153020/how-to-dump-a-collection-to-json-file-using-pymongo
+    countyPopulation = list(db.county_population.find({},{"population": 1, "_id":0, "county": 1}))
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_json.html
+    df = pd.DataFrame(countyPopulation)
+    countyPopJson = df.to_json(orient='records')
+    # print(countyPopJson)
+    
+    return countyPopJson
+
+def getCityPopulation():
+    db = getConnection()    
+    cityPopulation = list(db.city_population.find({},{"population": 1, "_id":0, "city": 1}))
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_json.html
+    df = pd.DataFrame(cityPopulation)
+    cityPopJson = df.to_json(orient='records')
+    # print(cityPopJson)
+    return cityPopJson
+
+getCountyPopulation()
+getCityPopulation()
